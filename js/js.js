d90ae3519163c4344694ed235f1179f07c7d382b
beadlejack <beadlejack@me.com>
2015-03-01 03:05:25 -0800

    cleaning up

--- js/js.js
+++ js/js.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function ($) {
  
+ 	//back to top button
+	var offset = 250;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTo: 0}, duration);
+        return false;
+    })
  
     //initialise Stellar.js
     $(window).stellar();
