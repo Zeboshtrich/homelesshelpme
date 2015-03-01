60491486a31e2a2e2bef0d545fbb0157c00c19ae
Zeboshtrich <nng@scu.edu>
2015-02-28 23:23:56 -0800

    geo

--- js/script.js
+++ js/script.js
@@ -6,16 +6,22 @@ var infos = Array();
 function initialize() {
     // prepare Geocoder
     geocoder = new google.maps.Geocoder();
-
+     if (status == google.maps.GeocoderStatus.OK){
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+     }
+     else{
     // intial position
     var myLatlng = new google.maps.LatLng(37.3544,-121.9692);
 
     var myOptions = { // default map options
         zoom: 14,
-        center: myLatlng,
+        center: pos,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+}
     map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+   
 }
 
 // clear overlays function
